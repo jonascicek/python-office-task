45e4207b7c944140fad660f21cf535c3f3b6a319
jonascicek <jonascicek98@gmail.com>
2024-10-04 14:27:36 +0200

    Show tasklist function added

--- python-office-task.py
+++ python-office-task.py
@@ -9,3 +9,13 @@ def add_task():
     task = input ("Please enter a task to be added to your task list")
     officetasklist.append(task)
     print(f"{task}has been added to your task list")
+
+# Show tasklist function
+
+def show_tasklist():
+    if officetasklist:
+        print("Your tasklist:")
+        for task in officetasklist:
+            print(f"{task}")
+    else:
+        print("Your tasklist is empty.")
