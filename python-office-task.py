f6bac0c1b85b8dea3fa5d0ac6e54566d2f54bbf9
jonascicek <jonascicek98@gmail.com>
2024-10-04 14:15:52 +0200

    Add task function

--- python-office-task.py
+++ python-office-task.py
@@ -1,1 +1,11 @@
 # python-office-task
+
+# Empty officetasklist
+officetasklist = []
+
+# Add task function
+
+def add_task():
+    task = input ("Please enter a task to be added to your task list")
+    officetasklist.append(task)
+    print(f"{task}has been added to your task list")
