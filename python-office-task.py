ff1fb62ea8b65f15615b82f764912d0d7525b929
jonascicek <jonascicek98@gmail.com>
2024-10-04 14:51:24 +0200

    Added remove function + main function

--- python-office-task.py
+++ python-office-task.py
@@ -19,3 +19,37 @@ def show_tasklist():
             print(f"{task}")
     else:
         print("Your tasklist is empty.")
+
+def remove_task():
+    task = input ("Please enter the completed task")
+    officetasklist.remove(task)
+    print(f"{task}removed from your tasklist.")
+
+# Main function
+
+def main():
+    while True:
+        print("---Your tasklist---")
+        print("1. Add a task")
+        print("2. Delete a task")
+        print("3. Show tasklist")
+        print("4. Exit tasklist")
+
+    # Save the input from the User with the choice variable
+
+        choice = input("Please enter (1/2/3/4)")
+        if choice == "1":
+            add_task()
+        elif choice == "2":
+            remove_task()
+        elif choice == "3":
+            show_tasklist()
+        elif choice == "4":
+            print("Closing Tasklist! Good Bye!")
+            break
+        else:
+            print("Invalid input. Please type 1, 2, 3 or 4")
+
+if __name__ == "__main__":
+    main()
+
